f1c8e7d34d9fb0eeb432505fece9aac846f9fbb8
Omprakash Sahani <Omprakash.Sahani7991@gmail.com>
2026-05-03 10:20:22 +0000

    Update benchmark graph for iterative backward

--- scripts/plot_benchmark.py
+++ scripts/plot_benchmark.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-graph_sizes = [50, 100, 200, 400]
-backward_times = [0.000072, 0.000167, 0.000370, 0.001140]
+graph_sizes = [100, 1000, 5000, 10000]
+backward_times = [0.000323, 0.002406, 0.010410, 0.115016]
 
 plt.figure(figsize=(7, 4))
 plt.plot(graph_sizes, backward_times, marker="o")
 plt.xlabel("Graph Size")
 plt.ylabel("Backward Time (seconds)")
-plt.title("Backward Pass Time vs Graph Size")
+plt.title("Backward Pass Time vs Graph Size (Iterative Traversal)")
 plt.grid(True)
 
 plt.savefig("results/backward_benchmark.png", dpi=150, bbox_inches="tight")
